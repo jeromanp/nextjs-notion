c3f8effc3a63d36d2e026206e2bee51a6b02e9e5
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-09-25 23:45:06 -0600

    add: text and style page Home

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,37 +2,48 @@ import Link from "next/link";
 
 export default function Home({ quote }) {
   return (
-    <main className="h-screen bg-green-900 flex flex-col items-center justify-between p-24">
-      <h1 className="p-24">Hola mundo</h1>
-      <p className="font-bold">{JSON.stringify(quote)}</p>
-      <div className="py-6">
-        <Link href={"/recipes"}>
-          <button type="" className="bg-red-600 rounded-lg p-2">
-            Recetas
-          </button>
-        </Link>
-      </div>
+    <main className="h-screen bg-green-900 justify-between p-12">
       <div>
-        <Link href={"/blogs"}>
-          <button type="" className="bg-red-600 rounded-lg p-2">
-            Blogs
-          </button>
-        </Link>
+        <h1 className="my-5 text-4xl">Bienvenido</h1>
+        <p className="items-start text-xl">
+          Este es un proyecto en donde se utiliza Next JS y Notion para poder
+          generar páginas estáticas a partir del contenido que se puede publicar
+          en <i>Notion</i>
+        </p>
+      </div>
+      {/* <p className="font-bold">{JSON.stringify(quote)}</p> */}
+      <div className="mx-auto my-5">
+        <p className="items-start text-xl my-5">
+          Por el momento tenemos los siguiente enlaces de prueba:
+        </p>
+        <div className="flex justify-center">
+          <Link href={"/recipes"}>
+            <button type="" className="bg-red-600 rounded-lg p-2 mx-5">
+              Recetas
+            </button>
+          </Link>
+
+          <Link href={"/blogs"}>
+            <button type="" className="bg-red-600 rounded-lg p-2 mx-5">
+              Blogs
+            </button>
+          </Link>
+        </div>
       </div>
     </main>
   );
 }
 
-export const getStaticProps = async () => {
-  // fetch data
-  // create static pages
-  const quote = await fetch("https://api.breakingbadquotes.xyz/v1/quotes").then(
-    (resp) => resp.json()
-  );
+// export const getStaticProps = async () => {
+//   // fetch data
+//   // create static pages
+//   const quote = await fetch("https://api.breakingbadquotes.xyz/v1/quotes").then(
+//     (resp) => resp.json()
+//   );
 
-  return {
-    props: {
-      quote,
-    },
-  };
-};
+//   return {
+//     props: {
+//       quote,
+//     },
+//   };
+// };
