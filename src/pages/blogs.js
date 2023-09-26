0e61bd09f66fa322c5a5f85a8bf64994b175d796
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-09-26 10:44:48 -0600

    add format text

--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -4,20 +4,25 @@ import slugify from "slugify";
 
 const BlogPage = ({ blogs }) => {
   return (
-    <div className="bg-green-900 flex flex-col items-center justify-between p-24">
-      <Link href={"/"}>
-        <button type="" className="bg-red-600 p-2 rounded-lg">
-          Atrás
-        </button>
-      </Link>
-      <h1 className="p-24">Nuestros Blogs:</h1>
-      {blogs.map((blog) => (
-        <p key={blog}>
-          <Link href={`/blogs/${slugify(blog).toLowerCase()}`}>
-            <p>{blog}</p>
-          </Link>
-        </p>
-      ))}
+    <div className="h-screen bg-green-900 justify-between p-24">
+      <div className="items start flex">
+        <Link href={"/"}>
+          <button type="" className="bg-red-600 p-2 rounded-lg">
+            Atrás
+          </button>
+        </Link>
+      </div>
+      <div>
+        <h1 className="p-12 text-4xl flex justify-center">Nuestros Blogs:</h1>
+        {blogs.map((blog) => (
+          <p key={blog}>
+            <Link href={`/blogs/${slugify(blog).toLowerCase()}`}>
+              <p className="text-xl">- {blog}</p>
+            </Link>
+          </p>
+        ))}
+      </div>
+
       {/* <pre className="mx-auto">{JSON.stringify(blogs, null, 2)}</pre> */}
     </div>
   );
