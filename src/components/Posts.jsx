2d6fa5c57510bb6dc78d9dbd2f1e0d80bc64e879
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-09-27 11:47:30 -0600

    add description

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,21 +1,28 @@
 import Image from "next/image";
 
 export default function Posts(props) {
-  const { title, description, content, bannerImage, bannerImageWidth, bannerImageHeight } =
-    props;
+  const {
+    title,
+    description,
+    content,
+    bannerImage,
+    bannerImageWidth,
+    bannerImageHeight,
+  } = props;
 
   return (
     <article className="w-full mb-10 flex flex-col items-center pt-20">
       <h1 className="text-6xl font-black text-white mb-8">{title}</h1>
       <p>{description}</p>
-      <Image
-        alt="Blog Image"
-        src={bannerImage}
-        // width={bannerImageWidth}
-        width={bannerImageWidth}
-        height={bannerImageHeight}
-        className="[width: 800px]!"
-      />
+      <div className="py-5">
+        <img
+          alt="Blog Image"
+          src={bannerImage}
+          width={bannerImageWidth}
+          height={bannerImageHeight}
+          className="[width: 800px]!"
+        />
+      </div>
       <div
         className="text-xl mt-4 max-w-3xl leading-10 prose prose-p:text-white prose-headings:text-white"
         dangerouslySetInnerHTML={{ __html: content }}
