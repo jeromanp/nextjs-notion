dbfdddcddd9ea6f0d0b159df4e6d4b31e940ed27
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-09-27 23:00:08 -0600

    modificated page dynamic to render page from Notio

--- src/pages/recipes/[slug].js
+++ src/pages/recipes/[slug].js
@@ -7,17 +7,16 @@ const Recipe = ({ recipe }) => {
   return (
     <div className="bg-green-900 justify-between p-12">
       <div className="flex items-start">
-        
-      <Link href={"/recipes"}>
-        <button type="" className="bg-red-600 p-2 rounded-lg">
-          Atrás
-        </button>
-      </Link>
+        <Link href={"/recipes"}>
+          <button type="" className="bg-red-600 p-2 rounded-lg">
+            Atrás
+          </button>
+        </Link>
       </div>
       <Recipes
         title={recipe.title}
         ingredients={recipe.ingredients}
-        method={recipe.method}
+        content={recipe.content}
       />
       {/* <pre className="mx-auto">{JSON.stringify(recipe, null, 2)}</pre> */}
     </div>
@@ -75,118 +74,54 @@ export const getStaticProps = async ({ params: { slug } }) => {
   });
 
   const title = page.child_page.title;
-  const ingredients = [];
-  const method = [];
+  let i = 1;
 
-  blocks.results.forEach((block) => {
-    if (block.type === "numbered_list_item") {
-      ingredients.push(block.numbered_list_item.rich_text[0].text.content);
-    }
+  const res = blocks.results
+    .map((block) => {
+      if (block.type === "paragraph") {
+        const parra = block.paragraph?.rich_text[0]?.text.content || "";
+        return parra + `<br/>`;
+      } else if (block.type === "heading_1") {
+        return `${block.heading_1?.rich_text[0]?.text.content || ""}` + `<br/>`;
+      } else if (block.type === "heading_2") {
+        return (
+          ` ${block.heading_2?.rich_text[0]?.text.content || ""}` + `<br/>`
+        );
+      } else if (block.type === "heading_3") {
+        return (
+          ` ${block.heading_3?.rich_text[0]?.text.content || ""}` + `<br/>`
+        );
+      } else if (block.type === "quote") {
+        return `> ${block.quote?.rich_text?.[0]?.text.content}` + `<br/>`;
+      } else if (block.type === "to_do") {
+        const checkbox = block.to_do?.checked ? "[x]" : "[ ]";
+        return `${checkbox} ${block.to_do?.rich_text[0]?.plain_text}` + `<br/>`;
+      } else if (block.type === "image") {
+        const imageUrl = block.image?.external.url || "";
+        return `<img src="${imageUrl}" alt="Image" />` + `<br/>`;
+      } else if (block.type === "bulleted_list_item") {
+        return (
+          `- ${block.bulleted_list_item?.rich_text[0]?.text.content}` + `<br/>`
+        );
+      } else if (block.type === "numbered_list_item") {
+        const numberedItem =
+          `${i++}. ${block.numbered_list_item?.rich_text[0]?.text.content}` +
+          `<br/>`;
+        return numberedItem;
+      }
+    })
+    .filter(Boolean);
 
-    if (block.type === "paragraph" && block.paragraph.rich_text[0]) {
-      method.push(block.paragraph.rich_text[0].text.content);
-    }
-  });
+  console.log(res);
 
   return {
     props: {
       recipe: {
         title,
-        ingredients,
-        method,
+        content: res,
       },
     },
   };
 };
 
 export default Recipe;
-
-// GET DATA FROM ID
-
-// import { Client } from "@notionhq/client";
-// import Link from "next/link";
-
-// const Recipe = ({ recipe }) => {
-//   return (
-//     <div className="container mx-auto py-5">
-//       <Link href={"/recipes"}>
-//         <button type="" className="bg-red-600 p-2 rounded-lg">
-//           Atrás
-//         </button>
-//       </Link>
-//       <h1 className="text-red-800 text-3xl">Recet: {recipe.title}</h1>
-//       <pre>{JSON.stringify(recipe, null, 2)}</pre>);
-//     </div>
-//   );
-// };
-
-// export const getStaticPaths = async () => {
-//   const notion = new Client({
-//     auth: process.env.NOTION_SECRET,
-//   });
-
-//   const data = await notion.blocks.children.list({
-//     block_id: process.env.PAGE_ID,
-//   });
-
-//   const paths = [];
-
-//   data.results.forEach((result) => {
-//     if (result.type === "child_page") {
-//       paths.push({
-//         params: {
-//           id: result.id,
-//         },
-//       });
-//     }
-//   });
-
-//   return {
-//     paths,
-//     fallback: false,
-//   };
-// };
-
-// export const getStaticProps = async ({ params: { id } }) => {
-//   // fetch details for recipe
-//   const notion = new Client({
-//     auth: process.env.NOTION_SECRET,
-//   });
-
-//   const page = await notion.pages.retrieve({
-//     page_id: id,
-//   });
-
-//   const blocks = await notion.blocks.children.list({
-//     block_id: id,
-//   });
-
-//   const title = page.properties.title.title[0].plain_text;
-//   const ingredients = [];
-//   const method = [];
-
-//   blocks.results.forEach((block) => {
-//     if (block.type === "numbered_list_item") {
-//       ingredients.push(block.numbered_list_item.rich_text[0].text.content);
-//     }
-
-//     // console.log("BLOCKS ====>",blocks.results[0]);
-
-//     if (block.type === "paragraph" && block.paragraph.rich_text[0]) {
-//       method.push(block.paragraph.rich_text[0].text.content);
-//     }
-//     console.log(block);
-//   });
-
-//   return {
-//     props: {
-//       recipe: {
-//         title,
-//         ingredients,
-//         method,
-//       },
-//     },
-//   };
-// };
-
-// export default Recipe;
