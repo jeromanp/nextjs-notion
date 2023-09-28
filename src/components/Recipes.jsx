df1ed081a87ad169e8dd5fd4a6058d6fadc2d0a9
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-09-27 22:59:50 -0600

    modificated componente for render page from Notion

--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -1,37 +1,17 @@
 import Image from "next/image";
 
 export default function Recipes(props) {
-  const { title, ingredients, method } = props;
+  const { title, content } = props;
+  const contentWithoutCommas = content.join('');
 
   return (
     <article className="h-full flex flex-col items-center pt-7">
       <h1 className="text-6xl font-black text-white mb-5">{title}</h1>
       <div className="text-xl mt-4">
-        <h2 className="text-3xl pb-4">Ingredientes</h2>
-        <ul className="ml-5">
-          {ingredients.map((ingredient, index) => (
-            <li key={index}>
-              <span className="font-bold">*</span> {ingredient}
-            </li>
-          ))}
-        </ul>
-
-        <h2 className="text-3xl pb-4">Preparación</h2>
-        <ol className="ml-5">
-          {method.map((step, index) => (
-            <li key={index}>
-              <span className="font-bold">{index + 1}.</span> {step}
-            </li>
-          ))}
-        </ol>
-        {/* <Image
-        alt="Blog Image"
-        src={bannerImage}
-        // width={bannerImageWidth}
-        width={800}
-        height={bannerImageHeight}
-        className="[width: 800px]!"
-      /> */}
+        <div
+          className="text-xl mt-4 max-w-3xl leading-10 prose prose-p:text-white prose-headings:text-white"
+          dangerouslySetInnerHTML={{ __html: contentWithoutCommas }}
+        ></div>
       </div>
     </article>
   );
