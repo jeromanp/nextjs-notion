18b1642dc770e19b30c72932d16aa67b38731613
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-09-28 22:18:22 -0600

    styled blog

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,31 +1,49 @@
 import Image from "next/image";
 
 export default function Posts(props) {
-  const {
-    title,
-    description,
-    content,
-    bannerImage,
-    bannerImageWidth,
-    bannerImageHeight,
-  } = props;
+  const { title, content, bannerImage, datePublic } = props;
 
   return (
-    <article className="w-full mb-10 flex flex-col items-center pt-20">
-      <h1 className="text-6xl font-black text-white mb-8">{title}</h1>
-      <div className="py-5">
-        <img
-          alt="Blog Image"
-          src={bannerImage}
-          width={bannerImageWidth}
-          height={bannerImageHeight}
-          className="[width: 800px]!"
-        />
-      </div>
+    <article>
       <div
-        className="text-xl mt-4 max-w-3xl leading-10 prose prose-p:text-white prose-headings:text-white text-justify"
-        dangerouslySetInnerHTML={{ __html: content }}
-      ></div>
+        class="mb-4 md:mb-0 w-full max-w-screen-md mx-auto relative"
+        style={{ height: "24em" }}
+      >
+        <div
+          class="absolute left-0 bottom-0 w-full h-full z-10"
+          style={{
+            backgroundImage:
+              "linear-gradient(180deg, transparent, rgba(0, 0, 0, 0.7))",
+          }}
+        ></div>
+        <img src={bannerImage} className="w-full h-full object-cover" />
+        <div class="p-4 absolute bottom-0 left-0 z-20">
+          <h2 class="text-5xl font-semibold text-gray-100 leading-tight">
+            {title}
+          </h2>
+          <div class="flex mt-3">
+            {/* Image profiule author */}
+            <img
+              src="https://randomuser.me/api/portraits/men/97.jpg"
+              class="h-10 w-10 rounded-full mr-2 object-cover"
+            />
+            {/* Image profiule author */}
+
+            <div>
+              <p class="font-semibold text-gray-200 text-sm"> Mike Sullivan </p>
+              <p class="font-semibold text-gray-400 text-xs"> {datePublic} </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="px-4 lg:px-0 mt-12 text-gray-700 max-w-screen-md mx-auto text-lg leading-relaxed">
+        <p class="pb-6">
+          <div
+            className="text-xl text-white mt-4 max-w-3xl leading-10 prose prose-p:text-white prose-headings:text-white text-justify"
+            dangerouslySetInnerHTML={{ __html: content }}
+          ></div>
+        </p>
+      </div>
     </article>
   );
 }
