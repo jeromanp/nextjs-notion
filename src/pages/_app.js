4e7274235f91365489f168d1e08740c25fe82dbd
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-09-29 00:27:54 -0600

    add link google icons

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,22 @@
 import '../styles/globals.css'
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* Encabezado global de la aplicación */}
+      <Head>
+        <title>Notion y NextJS</title>
+        <meta name="description" content="Renderizado de paginas de Notion" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0"
+        />
+      </Head>
+      {/* Renderiza el componente de la página actual */}
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
