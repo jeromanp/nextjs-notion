0d4ab0c942826a0085fca0b3d333cf3d79ad9e29
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-09-28 21:38:48 -0600

    formati quote

--- src/pages/blogs/[slug].js
+++ src/pages/blogs/[slug].js
@@ -57,22 +57,27 @@ export const getServerSideProps = async (context) => {
     block_id: pageId,
   });
 
-  // console.log(pageContent.results[23].toggle);
+  console.log(pageContent.results[23].toggle.rich_text[0]);
   let i = 1;
   // Mapear todos los bloques en pageContent.results
   const content = pageContent.results
     .map((block) => {
       if (block.type === "paragraph") {
-        const parra = block.paragraph.rich_text[0]?.text?.content || "";
+        const parra = block.paragraph.rich_text[0]?.plain_text || "";
         return parra + `<br/>`;
       } else if (block.type === "heading_1") {
         return ` ${block.heading_1.rich_text[0]?.text.content || ""}` + `<br/>`;
       } else if (block.type === "heading_2") {
-        return ` ${block.heading_2.rich_text[0]?.text.content || ""}` + `<br/>`;
+        return ` ${block.heading_2.rich_text[0]?.plain_text || ""}` + `<br/>`;
       } else if (block.type === "heading_3") {
-        return ` ${block.heading_3.rich_text[0]?.text.content || ""}` + `<br/>`;
+        return ` ${block.heading_3.rich_text[0]?.plain_text || ""}` + `<br/>`;
       } else if (block.type === "quote") {
-        return `> ${block.quote?.rich_text?.[0]?.text.content}` + `<br/>`;
+        const result = `> ${block.quote?.rich_text?.[0]?.plain_text}` + `<br/>`;
+        return `<blockquote className="p-4 my-4 border-l-4 border-gray-300 bg-gray-50 dark:border-gray-500 dark:bg-gray-800">
+            <p className="text-xl italic font-medium leading-relaxed text-gray-900 dark:text-white">
+              ${result}
+            </p>
+          </blockquote>`;
       } else if (block.type === "to_do") {
         const checkbox = block.to_do?.checked ? "[x]" : "[ ]";
         return `${checkbox} ${block.to_do?.rich_text[0]?.plain_text}` + `<br/>`;
@@ -81,11 +86,11 @@ export const getServerSideProps = async (context) => {
         return `<img src="${imageUrl}" alt="Image" />` + `<br/>`;
       } else if (block.type === "bulleted_list_item") {
         return (
-          `- ${block.bulleted_list_item?.rich_text[0]?.text.content}` + `<br/>`
+          `- ${block.bulleted_list_item?.rich_text[0]?.plain_text}` + `<br/>`
         );
       } else if (block.type === "numbered_list_item") {
         const numberedItem =
-          `${i++}. ${block.numbered_list_item?.rich_text[0]?.text.content}` +
+          `${i++}. ${block.numbered_list_item?.rich_text[0]?.plain_text}` +
           `<br/>`;
         return numberedItem;
       }
@@ -102,9 +107,10 @@ export const getServerSideProps = async (context) => {
       //   `;
       //   return toggleHtml;
       // }
-
-      //
-      else if (block.type === "video") {
+      else if (block.type === "toogle") {
+        const toggleTitle = block.toggle?.rich_text[0]?.plain_text;
+        // const toggleDescription =
+      } else if (block.type === "video") {
         const videoUrl = block.video.external.url || "";
         return (
           `<video src="${videoUrl}" controls width="640" height="360" preload="auto" />` +
