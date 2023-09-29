29ab7e7acc1521238571ad3f5dc8034e608b7bf2
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-09-29 12:34:09 -0600

    metadata page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,22 +1,16 @@
-import '../styles/globals.css'
-import Head from 'next/head';
+import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      {/* Encabezado global de la aplicación */}
       <Head>
         <title>Notion y NextJS</title>
         <meta name="description" content="Renderizado de paginas de Notion" />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0"
-        />
       </Head>
-      {/* Renderiza el componente de la página actual */}
       <Component {...pageProps} />
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
